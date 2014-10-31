73cd0235e467e000260fd971424d6dbca6505bb8
ZharkovIlia <zharkov1243@mail.ru>
2014-10-31 20:56:27 +0400

    <SolutionEquality> version with using the capabilities of scanf

--- SolutionEquality/solutionEquality.c
+++ SolutionEquality/solutionEquality.c
@@ -33,82 +33,10 @@ int main(void)
 }
 int isrightDouble(double* koef)
 {
-    double a = 0, base = 0;
-    int sign = 0, pos = BEFORE_SIGN;
-    char next_symbol = 0;
-    while ( ( (next_symbol = getchar() ) != ' ' || pos == BEFORE_SIGN) && next_symbol != '\n' )
-    {
-        switch (pos)
-        {
-            case BEFORE_SIGN:
-                if (next_symbol == '-') {
-                    sign = NEGATIVE;
-                    pos = BEFORE_DIGIT;
-                } else if ( (next_symbol - '0') >= 0 && (next_symbol - '0') <= 9 ) {
-                    sign = POSITIVE;
-                    a = a*10 + (next_symbol - '0');
-                    pos = DIGIT_BEFORE_POINT;
-                } else if (next_symbol == ' ') {
-                    ;    //nothing to do
-                } else {
-                    while (next_symbol != '\n') {
-                        next_symbol = getchar();
-                    }
-                    return DONT_RIGHT;
-                }
-                break;
-             case BEFORE_DIGIT:
-                if ( (next_symbol - '0') >= 0 && (next_symbol - '0') <= 9 ) {
-                    pos = DIGIT_BEFORE_POINT;
-                    a = a*10 + (next_symbol - '0');
-                } else {
-                    while (next_symbol != '\n') {
-                        next_symbol = getchar();
-                    }
-                    return DONT_RIGHT;
-                }
-                break;
-            case DIGIT_BEFORE_POINT:
-                if (next_symbol == '.') {
-                    pos = DIGIT_AFTER_POINT;
-                    base = 10;
-                } else if ( (next_symbol - '0') >= 0 && (next_symbol - '0') <= 9 ) {
-                    a = a*10 + (next_symbol - '0');
-                } else {
-                    while (next_symbol != '\n') {
-                        next_symbol = getchar();
-                    }
-                    return DONT_RIGHT;
-                }
-                break;
-            case DIGIT_AFTER_POINT:
-                if ( (next_symbol - '0') >= 0 && (next_symbol - '0') <= 9 ) {
-                    a += ((next_symbol - '0') / base);
-                    base *= 10;
-                } else {
-                    while (next_symbol != '\n') {
-                        next_symbol = getchar();
-                    }
-                    return DONT_RIGHT;
-                }
-                break;
-            default:   // case is impossible
-                break;
-        }
-    }
-    if ( pos == DIGIT_AFTER_POINT && base == 10) {
-        while (next_symbol != '\n') {
-            next_symbol = getchar();
-        }
+    int ch = 0;
+    if (scanf("%lf", koef) == 0) {
+        while ( (ch = getchar()) != '\n' );
         return DONT_RIGHT;
-    }
-    if (a == 0) {
-        *koef = 0;
-    } else {
-        *koef = (sign == POSITIVE)? a : -a;
-    }
-    while (next_symbol != '\n') {
-        next_symbol = getchar();
     }
     return RIGHT;
 }
@@ -131,7 +59,7 @@ void getCoefficients(double* a, double* b, double* c)
                 break;
         }
         while ( isrightDouble(&arr_coef[i]) == DONT_RIGHT ) {
-            printf("use standard sintax\n");
+            printf("use standard syntax\n");
         }
     }
     *a = arr_coef[0];
